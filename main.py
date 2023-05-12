1b065255d8bf7d96919d461196f33bb485fea57e
amine0395 <85034572+amine0395@users.noreply.github.com>
2023-05-12 23:34:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ from bs4 import BeautifulSoup
 # Function to check if a product name matches the search query
 def matches_query(name, query):
     name_words = name.text.strip().lower().split()
-    if name_words[0] != query[0].lower():
-        return False
-    if len(query) > 1 and len(name_words) < 2:
-        return False
-    if len(query) > 1 and name_words[1] != query[1].lower():
-        return False
-    if len(query) > 2 and len(name_words) < 3:
-        return False
-    if len(query) > 2 and name_words[2] != query[2].lower():
-        return False
+    z = name_words if len(name_words) < len(query) else query
+    i=0
+    for x in z:
+        if x!=name_words[i] and x!=query[i]:
+            return False
+        i+=1
     return True
 def matches_query1(name, query):
     name_words = name.strip().lower().split()
@@ -45,15 +41,21 @@ def extract_price1(price):
         return None
     numeric_str = match.group().replace(',', '')
     return float(numeric_str)
-
+def assemble(query):
+    b=""
+    for a in query:
+        b+=" "
+        b+=a
+    return b
 # Main function to scrape Jumia and CosmosElectro for products
 def scrape_jumia(jumia_url,base_jumia_url,query):
     columns = {'name': [], 'price': [], 'img url': []}
-
     jumia_r = requests.get(jumia_url )
     jumia_soup = BeautifulSoup(jumia_r.content, "html.parser")
     jumia_anchors = jumia_soup.find('div', {'class': '-paxs row _no-g _4cl-3cm-shs'}).find_all('article',{'class': 'prd _fb col c-prd'})
-    a= float(extract_price(jumia_anchors[0].find('a').find('div', {'class': 'info'}).find('div', {'class': 'prc'}).text))
+    if jumia_anchors:
+        a= 9999999
+
     for anchor in jumia_anchors:
             img = anchor.find('a',href=True)["href"]
             name = anchor.find('a').find('div', {'class': 'info'}).find('h3', {'class': 'name'})
@@ -66,42 +68,63 @@ def scrape_jumia(jumia_url,base_jumia_url,query):
                     columns['price'].append(price)
                     columns['img url'].append(base_jumia_url+str(img))
     return columns
-def scrape_cosmos(cosmos_url,query,columns):
+def scrape_cosmos(cosmos_url,query):
+    columns = {'name': [], 'price': [], 'img url': []}
     cosmos_r = requests.get(cosmos_url)
     cosmos_soup = BeautifulSoup(cosmos_r.content, "html.parser")
     cosmos_anchors = cosmos_soup.find_all(class_='col-xl-2 col-lg-4 col-md-4 col-sm-6 col-6')
+    if cosmos_anchors:
+        a=float(extract_price1(cosmos_anchors[0].find(class_='ps-product__price').string))
     for anchor in cosmos_anchors:
-        img = anchor.find('a', href=True)["href"]
-        price = anchor.find(class_='ps-product__price').string
-        name = anchor.find(class_='ps-product__title').string
 
+        img = anchor.find('a', href=True)["href"]
+        price = float(extract_price1(anchor.find(class_='ps-product__price').text))
+        name = anchor.find(class_='ps-product__title').string
         if matches_query(name, query):
-            columns['name'].append(name)
-            columns['price'].append(extract_price1(price))
-            columns['img url'].append(img)
+            if a > price:
+                a=price
+                columns = {'name': [], 'price': [], 'img url': []}
+                columns['name'].append(name)
+                columns['price'].append(price)
+                columns['img url'].append(img)
     return columns
-def scrape_bousfiha(bousfiha_url,query,columns):
+def scrape_bousfiha(bousfiha_url,query):
+    columns = {'name': [], 'price': [], 'img url': []}
     bousfiha_r = requests.get(bousfiha_url)
     bousfiha_soup = BeautifulSoup(bousfiha_r.content, 'html.parser')
     bousfiha_anchors = bousfiha_soup.find_all(class_="product-container")
+    if bousfiha_anchors:
+        a=9999999
     for anchor in bousfiha_anchors:
         img = anchor.find('a', href=True)["href"]
-        price = anchor.find(class_='price').string
+        price = float(convert_to_float(anchor.find(class_='price').string))
         name = anchor.find(class_='product-title').find('a', href=True).text
-        if matches_query1(name, query):
-            columns['name'].append(name)
-            columns['price'].append(convert_to_float(price))
-            columns['img url'].append(img)
+        if str(assemble(query)) in name.lower():
+            if a>=price:
+                a=price
+                columns = {'name': [], 'price': [], 'img url': []}
+                columns['name'].append(name)
+                columns['price'].append(price)
+                columns['img url'].append(img)
     return columns
 def scrape_products(query):
     columns = {'name': [], 'price': [], 'img url': []}
+    columns1 = {'name': [], 'price': [], 'img url': []}
     base_jumia_url='https://www.jumia.ma'
     jumia_url = base_jumia_url+'/catalog/?q=' + '+'.join(query) + '#catalog-listing&page='
     cosmos_url = 'https://www.cosmoselectro.ma/products?categories%5B%5D=0&q=' + '+'.join(query)
-    bousfiha_url="https://electrobousfiha.com/recherche?cat_id=all&controller=search&s="+str(query)+"&spr_submit_search=Search&n=21"
+    bousfiha_url="https://electrobousfiha.com/recherche?cat_id=all&controller=search&s="+str(query)+"&spr_submit_search=Search&n=21&order=product.price.asc"
     columns=scrape_jumia(jumia_url,base_jumia_url,query)
-    columns=scrape_cosmos(cosmos_url,query,columns)
-    columns=scrape_bousfiha(bousfiha_url,query,columns)
+    columns1=scrape_cosmos(cosmos_url,query)
+    if bool(columns1['name']):
+        columns['name'].append(columns1['name'][0])
+        columns['price'].append(columns1['price'][0])
+        columns['img url'].append(columns1['img url'][0])
+    columns1 = scrape_bousfiha(bousfiha_url, query)
+    if bool(columns1['name']):
+        columns['name'].append(columns1['name'][0])
+        columns['price'].append(columns1['price'][0])
+        columns['img url'].append(columns1['img url'][0])
     return columns
 if __name__ == '__main__':
     query = input("item name: ").lower().split()
