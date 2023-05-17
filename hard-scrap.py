2cd88313b0719fa4fd32a24275ab4c2d560fb53e
amine0395 <85034572+amine0395@users.noreply.github.com>
2023-05-18 00:01:55 +0200

    Update hard-scrap.py

--- hard-scrap.py
+++ hard-scrap.py
@@ -6,19 +6,15 @@ path="C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(path)
 def marjane():
     driver.get("https://www.marjanemall.ma/catalogsearch/result/index/?p=2&q=xiaomi+redmi&product_list_order=most_viewed")
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[2]/div[1]/div[2]/div/div[3]/ol")))
-        search = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[2]/div[1]/div[2]/div/div[3]/ol")
-        print("-------------------------------")
-        print(search)
-        i=0;
-        for item in search:
-            print("-------------------------------")
-            print(item)
-            price=item.find_element("/html/body/div[1]/main/div[2]/div[1]/div[2]/div/div[3]/ol/li["+i+"]/div/div/div[2]/div[1]/span/span/span")
-            i+=1
-            print(price.text)
-    except:
-        driver.quit()
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[2]/div[1]/div[2]/div/div[3]/ol")))
+    search = element.find_elements(By.CLASS_NAME,"product-item-info")
+    for item in search:
+        name=item.find_element(By.CLASS_NAME,"product-item-link").text
+        print(name)
+        link=item.find_element(By.CLASS_NAME,"product-item-link").get_attribute('href')
+        print(link)
+        item = item.find_element(By.CLASS_NAME,"price-wrapper")
+        price=item.get_attribute('data-price-amount')
+        print(price)
+    driver.quit()
 marjane()
