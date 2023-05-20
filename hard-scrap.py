3cb4e22d9195bc770ca431457b0294bbeec9a092
amine0395 <85034572+amine0395@users.noreply.github.com>
2023-05-21 00:20:39 +0200

    Update hard-scrap.py

--- hard-scrap.py
+++ hard-scrap.py
@@ -27,6 +27,25 @@ def scrape_marjane(marjmall_url,query):
                 columns['name'].append(name.text)
                 columns['price'].append(price)
                 columns['img url'].append(link)
+    return columns
+def scrape_electroplanet(ecplanet_url,query):
+    columns = {'name': [], 'price': [], 'img url': []}
+    driver.get(ecplanet_url)
+    element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/main/div/div/div/div/div[2]/div[2]/div[3]/div[1]/ol")))
+    search = element.find_elements(By.CLASS_NAME, "product-item-info")
+    if bool(search):
+        a=999999999.0
+    for item in search:
+        name = str(item.find_element(By.CLASS_NAME, "brand").text)+" "+str(item.find_element(By.CLASS_NAME, "ref").text)
+        link = item.find_element(By.CLASS_NAME, "product-item-link").get_attribute('href')
+        price = item.find_element(By.CLASS_NAME, "price").text
+        if True:
+            if float(price.replace(" ", "")) < a:
+                a=float(price.replace(" ", ""))
+                columns = {'name': [], 'price': [], 'img url': []}
+                columns['name'].append(name)
+                columns['price'].append(price.replace(" ", ""))
+                columns['img url'].append(link)
     driver.quit()
     return columns
-
